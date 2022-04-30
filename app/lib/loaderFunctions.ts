95b886e9aa8f72925a86ac24113f625a1d0bee57
Jakob Ankarhem <jakob.ankarhem@jetshop.se>
2022-04-30 11:09:28 +0200

    pass filters as route params

--- app/lib/loaderFunctions.ts
+++ app/lib/loaderFunctions.ts
@@ -3,6 +3,26 @@ import { RouteQuery } from '~/graphql/types';
 import { sendJetshopRequest } from './jetshop';
 import { FuncParams } from './utils/types';
 
+interface FilterInput {
+  listFilters: {
+    id: string;
+    values: string[];
+  }[];
+  rangeFilters: {
+    id: string;
+    min: number;
+    max: number;
+  }[];
+  booleanFilters: {
+    id: string;
+    value: boolean;
+  }[];
+  multiListFilters: {
+    id: string;
+    values: string[];
+  }[];
+}
+
 export const createRouteLoaderFunction =
   (
     props: Pick<FuncParams<typeof sendJetshopRequest>, 'query'> & {
@@ -18,6 +38,41 @@ export const createRouteLoaderFunction =
     const page = parseInt(url.searchParams.get('page') || '1');
     const offset = Math.max((page - 1) * props.variables.pageSize, 0);
 
+    // Filters
+    const filtersState = {
+      listFilters: {} as Record<string, any[]>,
+      // rangeFilters: {} as Record<string, ,
+      // booleanFilters: ,
+      // multiListFilters: {},
+    };
+    const params = new URLSearchParams(url.search);
+    for (const [key, value] of params) {
+      const [type, id] = key.split('_');
+      if (!id) continue;
+
+      switch (type) {
+        case 'ListFilter':
+          if (!filtersState.listFilters.hasOwnProperty(id)) {
+            filtersState.listFilters[id] = [];
+          }
+          filtersState.listFilters[id].push(value);
+        default:
+          continue;
+      }
+    }
+
+    const listFilters = Object.entries(filtersState.listFilters).map(
+      ([id, values]) => ({ id, values })
+    );
+    console.log(filtersState, listFilters);
+
+    const filterInput: FilterInput = {
+      listFilters: listFilters,
+      rangeFilters: [],
+      booleanFilters: [],
+      multiListFilters: [],
+    };
+
     const response = await sendJetshopRequest({
       args: args,
       query: props.query,
@@ -25,9 +80,12 @@ export const createRouteLoaderFunction =
         path: url.pathname,
         first: props.variables.pageSize,
         offset: offset,
+        filters: filterInput,
       },
     });
     const result = await response.json();
+
+    // console.log(result.data.route.object);
 
     const data: RouteQuery = result.data;
 
