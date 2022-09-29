2a700ea8ec82a5c1b2ee0d2de2b6f4b1f43abe90
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2022-09-29 23:09:21 +0200

    react 18 streaming

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,12 @@
 import 'dotenv/config';
-import { renderToString } from 'react-dom/server';
+import { PassThrough } from "stream";
+import { renderToPipeableStream } from "react-dom/server";
 import type { EntryContext } from 'remix';
 import { RemixServer } from 'remix';
+import isbot from "isbot";
+
+
+const ABORT_DELAY = 5000;
 
 export default function handleRequest(
   request: Request,
@@ -9,14 +14,52 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+  const callbackName = isbot(request.headers.get("user-agent"))
+  ? "onAllReady"
+  : "onShellReady";
 
-  responseHeaders.set('Content-Type', 'text/html');
+  return new Promise((resolve, reject) => {
+    let didError = false;
 
-  return new Response('<!DOCTYPE html>' + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        [callbackName]() {
+          let body = new PassThrough();
+
+          responseHeaders.set("Content-Type", "text/html");
+          responseHeaders.set("Transfer-Encoding", "chunked");
+          responseHeaders.set("Connection", "keep-alive");
+
+          resolve(
+            new Response(body as any, {
+              status: didError ? 500 : responseStatusCode,
+              headers: responseHeaders,
+            })
+          );
+          pipe(body);
+        },
+        onShellError(err) {
+          reject(err);
+        },
+        onError(error) {
+          didError = true;
+          console.error(error);
+        },
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
   });
+
+
+  // let markup = renderToPipeableStream(
+  //   <RemixServer context={remixContext} url={request.url} />
+  // );
+
+  // responseHeaders.set('Content-Type', 'text/html');
+
+  // return new Response('<!DOCTYPE html>' + markup, {
+  //   status: responseStatusCode,
+  //   headers: responseHeaders,
+  // });
 }
